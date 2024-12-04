6a4b46bd7e35eb9b67c30f56af998138c8bf0f54
lapq14 <luispetitqa@gmail.com>
2024-12-04 09:23:19 -0400

    esto

--- logintarea/settings.py
+++ logintarea/settings.py
@@ -152,6 +152,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'eugenionpetita@gmail.com'
-EMAIL_HOST_PASSWORD = 'bfthqgqbdmcrhmju'
-DEFAULT_FROM_EMAIL = 'el eqüipo'
+
