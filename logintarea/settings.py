391d4ce7bfc02c5c8f65cbf8b3999b15cb7b5044
Alejo16plus1 <hiperjesus17@gmail.com>
2024-12-03 21:18:48 -0400

    Agregación de cambio de contraseña, validación por correo, faltan preguntas de seguridad v2

--- logintarea/settings.py
+++ logintarea/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hiperjesus17@gmail.com'
-EMAIL_HOST_PASSWORD = 'nlyh gidt lomn lpnz'
+EMAIL_HOST_USER = 'tucorreo@gmail.com'
+EMAIL_HOST_PASSWORD = 'tuclave'
