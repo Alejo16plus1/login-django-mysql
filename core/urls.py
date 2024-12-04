555543a7bf79cf6db64f14c9dff349b7ef992522
lapq14 <luispetitqa@gmail.com>
2024-12-04 06:12:52 -0400

    añadido de rutas

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import home, cursos, exit, register, myprofile, activate  # Asegúrate de importar la vista activate
+from .views import home, cursos, exit, register, myprofile, activate, eleccion, pregunta, recuperar_preguntas, reestablecer_contrasena # Asegúrate de importar la vista activate
 from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', home, name='home'),
@@ -24,6 +25,10 @@ urlpatterns = [
     path('logout/', exit, name='exit'),
     path('register/', register, name='register'),
     path('myprofile/', myprofile, name='myprofile'),
+    path('eleccion/', eleccion, name='eleccion'),
+    path('pregunta/', pregunta, name='pregunta'),
+    path('recuperar_preguntas/<int:aux>/', recuperar_preguntas, name='recuperar_preguntas'),
+    path('reestablecer/', reestablecer_contrasena, name='reestablecer_contrasena'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
